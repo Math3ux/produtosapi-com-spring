c5256b9e241237d0b7028dcacac19a1a09931995
Matheus <matheusleidow@gmail.com>
2026-01-10 20:39:45 -0300

    Adição do application.yml

---
+++ src/test/java/io/github/math3ux/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.math3ux.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
